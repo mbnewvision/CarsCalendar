519d3857e8ec1f8af2930a58c06a73176cb2e8db
mbnewvision <mbnewvision@gmail.com>
2026-02-06 15:26:23 +0000

    Update Calendar.jsx

--- components/Calendar.jsx
+++ components/Calendar.jsx
@@ -1,2 +1,21 @@
-import FullCalendar from "@fullcalendar/react"; import dayGridPlugin from "@fullcalendar/daygrid";
-export default function Calendar(){ return (<div className="bg-white p-4 rounded-xl shadow"><FullCalendar plugins={[dayGridPlugin]} initialView="dayGridMonth"/></div>);}
+"use client";
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction";
+
+export default function Calendar({ events }) {
+  return (
+    <FullCalendar
+      plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+      initialView="dayGridMonth"
+      events={events}
+      height={600}
+      headerToolbar={{
+        left: "prev,next today",
+        center: "title",
+        right: "dayGridMonth,timeGridWeek,timeGridDay",
+      }}
+    />
+  );
+}
